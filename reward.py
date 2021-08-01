07578070b662472c15444730d36e5a2547686462
Haresh Kansara <kansarahar@gmail.com>
2021-08-01 01:23:55 -0400

    changed rewards to be normally distributed

--- reward.py
+++ reward.py
@@ -4,10 +4,10 @@ def reward_function(params):
 
   # ----------------------------------------------------- #
 
-  heading_weight = 35
+  heading_weight = 30
   steering_weight = 20
   center_weight = 35
-  speed_weight = 10
+  speed_weight = 15
 
   # ----------------------------------------------------- #
 
@@ -31,38 +31,40 @@ def reward_function(params):
   track_vector = waypoints[closest_waypoints[1]]-waypoints[closest_waypoints[0]]
   track_direction = np.arctan2(track_vector[1], track_vector[0])*180/np.pi
 
-
   # reward if heading direction is the same as track direction
   heading_difference = abs(track_direction-heading_direction)
   heading_difference = heading_difference if heading_difference < 180 else 360-heading_difference
 
-  heading_reward = ((45-heading_difference)/45)**2 if heading_difference < 45 else 0
-  heading_reward = heading_reward if heading_reward < 1 else 1
+  # when heading angle difference is 20, the reward is 1/e~0.37
+  heading_reward = np.exp(-0.0025*heading_difference**2) if heading_difference < 45 else 0
 
   # ----------------------------------------------------- #
 
   # reward if steering direction is the same as track direction
   steering_direction = steering_angle+heading_direction
-  steering_direction = steering_direction if steering_direction < 180 else 360-steering_direction
-  steering_difference = abs(track_direction-(steering_angle+heading_direction))
+  steering_difference = abs(track_direction-steering_direction)
   steering_difference = steering_difference if steering_difference < 180 else 360-steering_difference
 
-  steering_reward = ((45-steering_difference)/45)**2 if steering_difference < 45 else 0
-  steering_reward = steering_reward if steering_reward < 1 else 1
+  # when steering angle difference is 20, the reward is 1/e~0.37
+  steering_reward = np.exp(-0.0025*steering_difference**2) if steering_difference < 45 else 0
 
   # ----------------------------------------------------- #
 
   # reward if close to center line
-  center_reward = 1-distance_from_center/(track_width/2)
-  center_reward = center_reward if distance_from_center < track_width/2 else 0
-  center_reward = center_reward if center_reward < 1 else 1
+
+  # when at 50% of the track width, the reward is 1/e~0.37
+  center_reward = np.exp(-16*(distance_from_center/track_width)**2) if distance_from_center < track_width/2 else 0
 
   # ----------------------------------------------------- #
 
-  # reward if higher speed
-  speed_reward = speed/5
-  speed_reward = speed_reward if speed_reward < 1 else 1
+  # reward for speed
+  ideal_speed = 1
 
+  # when speed deviates from ideal_speed by 0.5, reward is 1/e~0.37
+  speed_reward = np.exp(-4*(ideal_speed-speed)**2) if speed > 0 else 0
+
+  # ----------------------------------------------------- #
+  
   # print('track_direction:', track_direction)
   # print('heading_direction:', heading_direction)
   # print('heading_difference:', heading_difference)
@@ -72,9 +74,7 @@ def reward_function(params):
   # print('center_reward:', center_reward)
   # print('speed_reward:', speed_reward)
 
-  # ----------------------------------------------------- #
-
   reward = heading_weight*heading_reward + steering_weight*steering_reward + center_weight*center_reward + speed_weight*speed_reward
-  reward = reward if reward > 0 else 1e-3
+  reward = reward if reward > 1e-3 else 1e-3
   print('rewards:', heading_reward, steering_reward, center_reward, speed_reward, reward)
   return float(reward)
