90d5d0de93c1eb4a321fbf49e3de117e971bcf05
Haresh Kansara <kansarahar@gmail.com>
2021-08-08 02:49:48 -0400

    added more plot options and changed reward function

--- reward.py
+++ reward.py
@@ -4,9 +4,8 @@ def reward_function(params):
 
   # --------------------------- Weights --------------------------- #
 
-  heading_weight = 30
-  steering_weight = 20
-  center_weight = 35
+  steering_weight = 55
+  center_weight = 30
   speed_weight = 15
 
   # ---------------------- Off-track Penalty ---------------------- #
@@ -19,7 +18,7 @@ def reward_function(params):
 
   speed = params['speed']
   steering_angle = params['steering_angle']
-  heading_direction = params['heading']
+  heading = params['heading']
   track_width = params['track_width']
   distance_from_center = params['distance_from_center']
   closest_waypoints = params['closest_waypoints']
@@ -27,26 +26,29 @@ def reward_function(params):
   
   # --------------------------- Heading --------------------------- #
 
+  # I don't think there's really any benefit to rewarding separately for
+  # steering and heading, so I will omit the heading reward
+
   # calculate track direction angle
   track_vector = waypoints[closest_waypoints[1]]-waypoints[closest_waypoints[0]]
   track_direction = np.arctan2(track_vector[1], track_vector[0])*180/np.pi
 
   # reward if heading direction is the same as track direction
-  heading_difference = abs(track_direction-heading_direction)
-  heading_difference = heading_difference if heading_difference < 180 else 360-heading_difference
+  # heading_difference = abs(track_direction-heading)
+  # heading_difference = heading_difference if heading_difference < 180 else 360-heading_difference
 
-  # when heading angle difference is 20, the reward is 1/e~0.37
-  heading_reward = np.exp(-0.0025*heading_difference**2) if heading_difference < 45 else 0
+  # # when heading angle difference is 20, the reward is 1/e~0.37
+  # heading_reward = np.exp(-0.0025*heading_difference**2) if heading_difference < 45 else 0
 
   # --------------------------- Steering --------------------------- #
 
   # reward if steering direction is the same as track direction
-  steering_direction = steering_angle+heading_direction
+  steering_direction = steering_angle+heading
   steering_difference = abs(track_direction-steering_direction)
   steering_difference = steering_difference if steering_difference < 180 else 360-steering_difference
 
-  # when steering angle difference is 20, the reward is 1/e~0.37
-  steering_reward = np.exp(-0.0025*steering_difference**2) if steering_difference < 45 else 0
+  # when steering difference is 16, the reward is 1/e~0.37
+  steering_reward = np.exp(-0.004*steering_difference**2) if steering_difference < 30 else 0
 
   # --------------------------- Center --------------------------- #
 
@@ -66,15 +68,14 @@ def reward_function(params):
   # --------------------------- Total --------------------------- #
   
   # print('track_direction:', track_direction)
-  # print('heading_direction:', heading_direction)
-  # print('heading_difference:', heading_difference)
+  # print('heading:', heading)
   # print('steering_difference:', steering_difference)
-  # print('heading_reward:', heading_reward)
   # print('steering_reward:', steering_reward)
   # print('center_reward:', center_reward)
   # print('speed_reward:', speed_reward)
 
-  reward = heading_weight*heading_reward + steering_weight*steering_reward + center_weight*center_reward + speed_weight*speed_reward
+  reward = steering_weight*steering_reward + center_weight*center_reward + speed_weight*speed_reward
   reward = reward if reward > 1e-3 else 1e-3
-  print('rewards:', heading_reward, steering_reward, center_reward, speed_reward, reward)
+  print('rewards:', steering_reward, center_reward, speed_reward, reward)
+  print('car stats:', params['x'], params['y'], speed, steering_angle, heading)
   return float(reward)
--- utils/plot_track.py
+++ utils/plot_track.py
@@ -15,31 +15,28 @@ import matplotlib.pyplot as plt
 dir_name = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser(description='A tool used to process waypoints obtained from a .npy file')
-parser.add_argument('--track_name', type=str, nargs=1, help='name of the track - example: reinvent2018', required=True)
-parser.add_argument('--img_name', type=str, nargs=1, help='name of the output image - example: track.png', default=['track.png'])
-parser.add_argument('--number', action='store_true', help='displays the number of the waypoint next to every 10 waypoints')
-parser.add_argument('--line', action='store_true', help='plot as line graph instead of scatter')
+parser.add_argument('--track_name', type=str, nargs=1, help='REQUIRED - name of the track, example: reinvent2018', required=True)
+parser.add_argument('--img_name', type=str, nargs=1, help='OPTIONAL - name of the output image, default: track.png', default=['track.png'])
+parser.add_argument('--number', action='store_true', help='OPTIONAL - displays the number of the waypoint next to every 10 waypoints')
+parser.add_argument('--meter', action='store_true', help='OPTIONAL - displays the meters traveled across the track')
+parser.add_argument('--line', action='store_true', help='OPTIONAL - plot as line graph instead of scatter')
 
 args = parser.parse_args()
 
-track_name, img_name, show_numbers, line_graph = args.track_name[0], args.img_name[0], args.number, args.line
+track_name, img_name, show_numbers, show_meters, line_graph = args.track_name[0], args.img_name[0], args.number, args.meter, args.line
 
-# --------------------------- Plot --------------------------- #
+# ------------------------ Plot Main ------------------------- #
 
 # Get points from .npy files
 waypoints, rightpoints, leftpoints = [np.load('%s/../tracks/track_%s/%s.npy' % (dir_name, points_set, track_name)) for points_set in ['waypoints', 'rightpoints', 'leftpoints']]
 waypoints, rightpoints, leftpoints = np.array(waypoints), np.array(rightpoints), np.array(leftpoints)
-
 colors = {
   'waypoints': '#6d6dff',
   'rightpoints': '#ff6d6d',
-  'leftpoints': '#6dff6d'
+  'leftpoints': '#6dff6d',
+  'metermarker': '#ffa500',
+  'indexmarker': '#000000'
 }
-
-if show_numbers:
-  for i in range(len(waypoints)):
-    if i % 10 == 0:
-      plt.text(waypoints[i][0] * (1 + 0.01), waypoints[i][1] * (1 + 0.01) , i, fontsize=10)
 
 if not line_graph:
   # Plot waypoints as scatter
@@ -52,6 +49,25 @@ else:
   plt.plot([point[0] for point in waypoints], [point[1] for point in waypoints], c=colors['waypoints'])
   plt.plot([point[0] for point in rightpoints], [point[1] for point in rightpoints], c=colors['rightpoints'])
   plt.plot([point[0] for point in leftpoints], [point[1] for point in leftpoints], c=colors['leftpoints'])
+
+# ----------------------- Plot Options ----------------------- #
+
+total_dist = 0
+marker_dist = 0
+for i in range(len(waypoints)):
+  if show_numbers:
+    if i % 10 == 0:
+      plt.text(waypoints[i][0] * 1.02, waypoints[i][1] * 1.02 , i, fontsize=10)
+      plt.scatter(waypoints[i][0], waypoints[i][1], c=colors['indexmarker'])
+  if show_meters and i+1 < len(waypoints):
+    if marker_dist >= 1 or i == 0:
+      marker_dist -= 1 if i > 0 else 0
+      plt.text(waypoints[i][0] * 1.02, waypoints[i][1] * 1.02, '%sm' % int(total_dist), fontsize=10)
+      plt.scatter(waypoints[i][0], waypoints[i][1], c=colors['metermarker'])
+    total_dist += np.linalg.norm(waypoints[i+1]-waypoints[i])
+    marker_dist += np.linalg.norm(waypoints[i+1]-waypoints[i])
+
+# ------------------------ Save Image ------------------------ #
 
 os.makedirs('%s/plots' % os.path.abspath(os.path.join(dir_name, '..')), exist_ok=True)
 plt.savefig('%s/../plots/%s' % (dir_name, img_name))
